0583dd5dbd5929cfb7dd0340165cde9890ebc036
Toufik Saouchi <toufik.saouchi@gmail.com>
2026-04-05 15:45:30 +0200

    add github dir

--- python/python_archetype.sh
+++ python/python_archetype.sh
@@ -8,6 +8,7 @@ cd "$TARGET_DIR"
 set -euo pipefail
 
 PROJECT_NAME=$2
+PACKAGE_NAME=$3
 
 echo "Creating project: $PROJECT_NAME"
 
@@ -17,13 +18,13 @@ cd "$PROJECT_NAME"
 
 # Core directories
 mkdir -p \
-  src/domain/model \
-  src/domain/service \
-  src/application/use_cases \
-  src/application/port/inbound \
-  src/application/port/outbound \
-  src/adapter/inbound \
-  src/adapter/outbound \
+  src/$PACKAGE_NAME/domain/model \
+  src/$PACKAGE_NAME/domain/service \
+  src/$PACKAGE_NAME/application/use_cases \
+  src/$PACKAGE_NAME/application/port/inbound \
+  src/$PACKAGE_NAME/application/port/outbound \
+  src/$PACKAGE_NAME/adapter/inbound \
+  src/$PACKAGE_NAME/adapter/outbound \
   tests \
   docker \
   scripts
@@ -32,7 +33,7 @@ mkdir -p \
 find src -type d -exec touch {}/__init__.py \;
 
 # Main entrypoint
-cp $SCRIPT_DIR/main.py $TARGET_DIR/$PROJECT_NAME/src
+cp $SCRIPT_DIR/main.py $TARGET_DIR/$PROJECT_NAME
 
 # pyproject.toml (uv-ready minimal config)
 cp $SCRIPT_DIR/pyproject.toml $TARGET_DIR/$PROJECT_NAME
@@ -42,6 +43,9 @@ cp $SCRIPT_DIR/Makefile $TARGET_DIR/$PROJECT_NAME
 
 # gitignore
 cp $SCRIPT_DIR/gitignore $TARGET_DIR/$PROJECT_NAME/.gitignore
+
+# github
+cp -r $SCRIPT_DIR/github $TARGET_DIR/$PROJECT_NAME/.github
 
 # LICENSE
 cp $SCRIPT_DIR/MIT-LICENSE $TARGET_DIR/$PROJECT_NAME/LICENSE
